9fff87df2bc3b9dcc2ebb59a19aa60c77edbcbdf
Ricky McConnell <mr.mcconnell12@yahoo.com>
2020-02-17 12:56:20 -0600

    created models -- setting up identity and entity frameworks

--- PersonalJournal/Data/ApplicationDbContext.cs
+++ PersonalJournal/Data/ApplicationDbContext.cs
@@ -1,16 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PersonalJournal.Models;
 
 namespace PersonalJournal.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<PersonalJournal.Models.Entry> Entry { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
+            // Intitial user seeding 
+            modelBuilder.Entity<Entry>()
+                .Property(d => d.DateCreated)
+                .HasDefaultValueSql("getdate()");
+
+            ApplicationUser user = new ApplicationUser
+            {
+                FirstName = "Admin",
+                LastName = "Strator",
+                Email = "adminstrator@internet.com",
+                NormalizedEmail = "ADMINSTRATOR@INTERNET.COM",
+                EmailConfirmed = true,
+                LockoutEnabled = false,
+                SecurityStamp = "7f434309-a4d9-48e9-9ebb-8803db794577",
+                Id = "00000000-ffff-ffff-ffff-ffffffffffff"
+            };
+            var passwordHash = new PasswordHasher<ApplicationUser>();
+            user.PasswordHash = passwordHash.HashPassword(user, "Abc123!");
+            modelBuilder.Entity<ApplicationUser>().HasData(user);
+
+
         }
     }
 }
