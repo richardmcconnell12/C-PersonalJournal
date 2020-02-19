c1dd8d3e6d1cad49f534959d80580c526306a7c7
Ricky McConnell <mr.mcconnell12@yahoo.com>
2020-02-19 10:25:30 -0600

    reseeded database and created entriesController

---
+++ PersonalJournal/Controllers/EntriesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PersonalJournal.Data;
+using PersonalJournal.Models;
+
+namespace PersonalJournal.Controllers
+{
+    public class EntriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EntriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Entries
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Entries.Include(e => e.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Entries/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var entry = await _context.Entries
+                .Include(e => e.User)
+                .FirstOrDefaultAsync(m => m.EntryId == id);
+            if (entry == null)
+            {
+                return NotFound();
+            }
+
+            return View(entry);
+        }
+
+        // GET: Entries/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id");
+            return View();
+        }
+
+        // POST: Entries/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EntryId,Mood,Entries,UserId,DateCreated")] Entry entry)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(entry);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", entry.UserId);
+            return View(entry);
+        }
+
+        // GET: Entries/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var entry = await _context.Entries.FindAsync(id);
+            if (entry == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", entry.UserId);
+            return View(entry);
+        }
+
+        // POST: Entries/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int? id, [Bind("EntryId,Mood,Entries,UserId,DateCreated")] Entry entry)
+        {
+            if (id != entry.EntryId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(entry);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EntryExists(entry.EntryId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", entry.UserId);
+            return View(entry);
+        }
+
+        // GET: Entries/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var entry = await _context.Entries
+                .Include(e => e.User)
+                .FirstOrDefaultAsync(m => m.EntryId == id);
+            if (entry == null)
+            {
+                return NotFound();
+            }
+
+            return View(entry);
+        }
+
+        // POST: Entries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            var entry = await _context.Entries.FindAsync(id);
+            _context.Entries.Remove(entry);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EntryExists(int? id)
+        {
+            return _context.Entries.Any(e => e.EntryId == id);
+        }
+    }
+}
