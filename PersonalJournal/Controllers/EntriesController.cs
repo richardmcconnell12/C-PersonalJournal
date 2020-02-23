57aa2afb3ae854cb933715fefd84b274905ffb3b
Ricky McConnell <mr.mcconnell12@yahoo.com>
2020-02-23 11:26:59 -0600

    Error with matching end points -- working on it

--- PersonalJournal/Controllers/EntriesController.cs
+++ PersonalJournal/Controllers/EntriesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -13,11 +14,15 @@ namespace PersonalJournal.Controllers
     public class EntriesController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public EntriesController(ApplicationDbContext context)
+        public EntriesController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
+
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
 
         // GET: Entries
         public async Task<IActionResult> Index()
